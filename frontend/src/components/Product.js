a09d2189cfc94007a900b9ae50c2577325aa240a
wothag <33983572+wothag@users.noreply.github.com>
2020-12-15 16:52:22 +0100

    Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,30 +1,29 @@
 import React from 'react'
-import {Card} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
-const Product = ({product}) => {
-    return (
-      <Card className ="my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}>
+const Product = ({ product }) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
         </a>
 
-        <Card.Body>
-          <a href={`/product/${product._id}`}>
-            <Card.Title as='div'>
-              <strong>{product.name}</strong>
-            </Card.Title>  
-          </a>
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} avis `} />
+        </Card.Text>
 
-          <Card.Text as='div'>
-            <div className='my-3'>
-              {product.rating} sur {product.numReviews} Avis clients
-            </div>
-          </Card.Text>
-
-          <Card.Text as='h3'>€{product.price}</Card.Text>
-        </Card.Body> 
-      </Card>
-    )
+        <Card.Text as='h3'>€{product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default Product
