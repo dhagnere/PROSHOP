7d40440b6088401ec55cc1a0f85ee0e7c596bc61
wothag <33983572+wothag@users.noreply.github.com>
2020-12-17 10:18:59 +0100

    implementing propTypes on Rating component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
-const Rating = ({ value, text }) => {
+const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
       <span>
         <i
+          style={{ color }}
           className={
             value >= 1
               ? 'fas fa-star'
@@ -17,6 +18,7 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color }}
           className={
             value >= 2
               ? 'fas fa-star'
@@ -28,6 +30,7 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color }}
           className={
             value >= 3
               ? 'fas fa-star'
@@ -39,6 +42,7 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color }}
           className={
             value >= 4
               ? 'fas fa-star'
@@ -50,6 +54,7 @@ const Rating = ({ value, text }) => {
       </span>
       <span>
         <i
+          style={{ color }}
           className={
             value >= 5
               ? 'fas fa-star'
@@ -59,10 +64,20 @@ const Rating = ({ value, text }) => {
           }
         ></i>
       </span>
-      <span>  </span>
+      <span> </span>
       <span>{text ? text : ''}</span>
     </div>
   )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
 }
 
 export default Rating
